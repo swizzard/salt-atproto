3e26272923a7c9ed5b76254f1d1d2d4ac0bf1cc4
Sam Raker <sam.raker@gmail.com>
2025-07-15 13:08:12 -0400

    rename

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 // did:plc:zylhqsjug3f76uqxguhviqka
-use lexicon_pruner::{AppError, atproto, dns};
+use salt_atproto::{AppError, atproto, dns};
 #[tokio::main]
 async fn main() -> Result<(), AppError> {
     let atproto_client = atproto::client();
     let dns_client = dns::dns_client().await;
-    let mut cache = lexicon_pruner::_Cache::default();
+    let mut cache = salt_atproto::_Cache::default();
     let did =
         atrium_api::types::string::Did::new("did:plc:xydueznukwpv3esjwcexd676".into()).unwrap();
     let outcome =
-        lexicon_pruner::check_user_collections(&mut cache, &dns_client, &atproto_client, &did)
+        salt_atproto::check_user_collections(&mut cache, &dns_client, &atproto_client, &did)
             .await?;
     println!("outcome:\n{outcome}");
     // let mut client = dns::dns_client().await;
